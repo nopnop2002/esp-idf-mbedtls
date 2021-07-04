e2000dfb877216da550e0f79dc3b32615b6c70a6
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-07-04 10:53:55 +0900

    Added calcBase64DecodedSize

--- base64/README.md
+++ base64/README.md
@@ -1,14 +1,15 @@
 # BASE64 Encode/Decode
 
 ```
-I (320) BASE64: 0x3ffb57c0   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
-I (330) BASE64: 0x3ffb57d0   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
-I (340) BASE64: EncodedSize=44
-I (340) BASE64: encode_len=44
-I (350) BASE64: 0x3ffaffac   41 41 45 43 41 77 51 46  42 67 63 49 43 51 6f 4c  |AAECAwQFBgcICQoL|
-I (360) BASE64: 0x3ffaffbc   44 41 30 4f 44 78 41 52  45 68 4d 55 46 52 59 58  |DA0ODxAREhMUFRYX|
-I (370) BASE64: 0x3ffaffcc   47 42 6b 61 47 78 77 64  48 68 38 3d              |GBkaGxwdHh8=|
-I (380) BASE64: decode_len=32
-I (380) BASE64: 0x3ffb57e4   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
-I (390) BASE64: 0x3ffb57f4   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (303) BASE64: 0x3ffb5800   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (313) BASE64: 0x3ffb5810   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (323) BASE64: EncodedSize=44
+I (323) BASE64: encode_len=44
+I (333) BASE64: 0x3ffaffac   41 41 45 43 41 77 51 46  42 67 63 49 43 51 6f 4c  |AAECAwQFBgcICQoL|
+I (343) BASE64: 0x3ffaffbc   44 41 30 4f 44 78 41 52  45 68 4d 55 46 52 59 58  |DA0ODxAREhMUFRYX|
+I (353) BASE64: 0x3ffaffcc   47 42 6b 61 47 78 77 64  48 68 38 3d              |GBkaGxwdHh8=|
+I (353) BASE64: DecodedSize=33
+I (363) BASE64: decode_len=32
+I (363) BASE64: 0x3ffb6918   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (373) BASE64: 0x3ffb6928   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
 ```
--- base64/main/main.c
+++ base64/main/main.c
@@ -12,22 +12,18 @@
 static const char *TAG = "BASE64";
 
 // Calculate the size after conversion to base64
-// http://akabanessa.blog73.fc2.com/blog-entry-83.html
-int32_t calcBase64EncodedSize(int origDataSize)
+// https://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
+int32_t calcBase64EncodedSize(int input_length)
 {
-	// 6bit単位のブロック数（6bit単位で切り上げ）
-	// Number of blocks in 6-bit units (rounded up in 6-bit units)
-	int32_t numBlocks6 = ((origDataSize * 8) + 5) / 6;
-	// 4文字単位のブロック数（4文字単位で切り上げ）
-	// Number of blocks in units of 4 characters (rounded up in units of 4 characters)
-	int32_t numBlocks4 = (numBlocks6 + 3) / 4;
-	// 改行を含まない文字数
-	// Number of characters without line breaks
-	int32_t numNetChars = numBlocks4 * 4;
-	// 76文字ごとの改行（改行は "\r\n" とする）を考慮したサイズ
-	// Size considering line breaks every 76 characters (line breaks are "\ r \ n")
-	//return numNetChars + ((numNetChars / 76) * 2);
-	return numNetChars;
+	int32_t output_length = 4 * ((input_length + 2) / 3);
+	return output_length;
+}
+
+// Calculate the size after conversion from base64
+int32_t calcBase64DecodedSize(int input_length)
+{
+	int32_t output_length = input_length / 4 * 3;;
+	return output_length;
 }
 
 void app_main()
@@ -40,30 +36,41 @@ void app_main()
 	int32_t EncodedSize = calcBase64EncodedSize(sizeof(src_buffer));
 	ESP_LOGI(TAG, "EncodedSize=%d", EncodedSize);
 
-	// Allocate memory
-	unsigned char* encord_buffer = NULL;
-	encord_buffer = malloc(EncodedSize+1);
-	if (encord_buffer == NULL) {
-		ESP_LOGE(TAG, "malloc fail. encord_buffer %d", EncodedSize);
+	// Allocate encode memory
+	unsigned char* encode_buffer = NULL;
+	// encode is null-terminated.
+	// Must be +1
+	encode_buffer = malloc(EncodedSize+1);
+	if (encode_buffer == NULL) {
+		ESP_LOGE(TAG, "malloc fail. encode_buffer %d", EncodedSize);
 		while(1) { vTaskDelay(1); }
 	}
 
 	// Encode to Base64
-	size_t encord_len;
-	//int ret = mbedtls_base64_encode(encord_buffer, EncodedSize, &encord_len, src_buffer, sizeof(src_buffer));
-	int ret = mbedtls_base64_encode(encord_buffer, EncodedSize+1, &encord_len, src_buffer, sizeof(src_buffer));
+	size_t encode_len;
+	int ret = mbedtls_base64_encode(encode_buffer, EncodedSize+1, &encode_len, src_buffer, sizeof(src_buffer));
 	if (ret != 0) {
 		ESP_LOGE(TAG, "Error in mbedtls encode! ret = -0x%x", -ret);
 		while(1) { vTaskDelay(1); }
 	}
+	ESP_LOGI(TAG, "encode_len=%d", encode_len);
+	ESP_LOG_BUFFER_HEXDUMP(TAG, encode_buffer, encode_len, ESP_LOG_INFO);
+
+	// Calculate the size after conversion from base64
+	int32_t DecodedSize = calcBase64DecodedSize(EncodedSize);
+	ESP_LOGI(TAG, "DecodedSize=%d", DecodedSize);
+
+	// Allocate decode memory
+	unsigned char* decode_buffer = NULL;
+	decode_buffer = malloc(DecodedSize);
+	if (decode_buffer == NULL) {
+		ESP_LOGE(TAG, "malloc fail. decode_buffer %d", EncodedSize);
+		while(1) { vTaskDelay(1); }
+	}
 
 	// Decode from Base64
-	unsigned char decode_buffer[64];
 	size_t decode_len;
-	ESP_LOGI(TAG, "encode_len=%d", encord_len);
-	ESP_LOG_BUFFER_HEXDUMP(TAG, encord_buffer, encord_len, ESP_LOG_INFO);
-
-	ret = mbedtls_base64_decode( decode_buffer, sizeof(decode_buffer), &decode_len, encord_buffer, encord_len);
+	ret = mbedtls_base64_decode( decode_buffer, DecodedSize, &decode_len, encode_buffer, encode_len);
 	if (ret != 0) {
 		ESP_LOGE(TAG, "Error in mbedtls decode! ret = -0x%x", -ret);
 		while(1) { vTaskDelay(1); }
@@ -71,5 +78,6 @@ void app_main()
 	ESP_LOGI(TAG, "decode_len=%d", decode_len);
 	ESP_LOG_BUFFER_HEXDUMP(TAG, decode_buffer, decode_len, ESP_LOG_INFO);
 
-	if (encord_buffer != NULL) free(encord_buffer);
+	if (encode_buffer != NULL) free(encode_buffer);
+	if (decode_buffer != NULL) free(decode_buffer);
 }
