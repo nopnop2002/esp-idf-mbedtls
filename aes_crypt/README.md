1255fd124b6fc852958ad9bdce2cda404ef8ec4d
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-02-04 00:41:39 +0900

    Added aes_crypt

---
+++ aes_crypt/README.md
@@ -0,0 +1,56 @@
+# AES encryption & decryption
+
+I referred [this](https://tls.mbed.org/kb/how-to/encrypt-with-aes-cbc).   
+
+# Select cryption   
+![config-top](https://user-images.githubusercontent.com/6020549/152373400-3082f5c5-af50-482b-860e-3c534c2ac0fc.jpg)
+![config-aes-1](https://user-images.githubusercontent.com/6020549/152373405-e143c9d4-5fde-424f-9954-78efd7139714.jpg)
+![config-aes-2](https://user-images.githubusercontent.com/6020549/152374798-032efd84-0819-4d9a-a743-97ac31ad2f75.jpg)
+
+
+
+# AES-CFB128   
+```
+I (0) CFB128: mbedtls_ctr_drbg_seed ok
+I (0) CFB128: mbedtls_ctr_drbg_random ok
+I (10) CFB128: mbedtls_aes_crypt_cfb128(MBEDTLS_AES_ENCRYPT) ok
+I (20) CFB128: 0x3ffb7cc6   35 fa fa 1b bf bc 06 43  81 96 3e db f6 81 ae ea  |5......C..>.....|
+I (30) CFB128: 0x3ffb7cd6   d1 94 18 05 17 ed 39 0a  64 3d f4 e6 45 a3 b7 cc  |......9.d=..E...|
+I (30) CFB128: 0x3ffb7ce6   a6 93 12 7d 12 3e 6c 61  ec 4e 8f 4d 7c 96 2b d4  |...}.>la.N.M|.+.|
+I (40) CFB128: 0x3ffb7cf6   68 3b                                             |h;|
+I (50) CFB128: mbedtls_aes_crypt_cfb128(MBEDTLS_AES_DECRYPT) ok
+I (60) CFB128: 0x3ffb7c46   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (70) CFB128: 0x3ffb7c56   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (80) CFB128: 0x3ffb7c66   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !"#$%&'()*+,-./|
+I (90) CFB128: 0x3ffb7c76   30 31                                             |01|
+```
+
+
+# AES-CBC   
+This must be a multiple of the block size(16*n Bytes).
+```
+I (0) CBC: mbedtls_ctr_drbg_seed ok
+I (0) CBC: mbedtls_ctr_drbg_random ok
+I (10) CBC: mbedtls_aes_crypt_cbc(MBEDTLS_AES_ENCRYPT) ok
+I (20) CBC: 0x3ffb7cc6   46 4e af 6f 79 6e 49 64  f6 d1 ed 2c d7 04 df d9  |FN.oynId...,....|
+I (20) CBC: 0x3ffb7cd6   2a 22 48 e3 87 a3 20 0c  06 7d 8a 77 2c a3 18 c1  |*"H... ..}.w,...|
+I (30) CBC: 0x3ffb7ce6   17 a7 c9 37 4d 6e 78 28  a3 26 52 be ab 78 c9 8e  |...7Mnx(.&R..x..|
+I (40) CBC: 0x3ffb7cf6   ae 8c 3d 24 0a 9b 0c 6e  f6 25 20 d8 d1 1a c1 14  |..=$...n.% .....|
+I (50) CBC: mbedtls_aes_crypt_cbc(MBEDTLS_AES_DECRYPT) ok
+I (60) CBC: 0x3ffb7c46   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (70) CBC: 0x3ffb7c56   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (80) CBC: 0x3ffb7c66   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !"#$%&'()*+,-./|
+I (90) CBC: 0x3ffb7c76   30 31 32 33 34 35 36 37  38 39 3a 3b 3c 3d 3e 3f  |0123456789:;<=>?|
+```
+
+
+# AES-ECB   
+AES single-block encryption or decryption.   
+```
+I (0) ECB: mbedtls_ctr_drbg_seed ok
+I (0) ECB: mbedtls_ctr_drbg_random ok
+I (10) ECB: mbedtls_aes_crypt_ecb(MBEDTLS_AES_ENCRYPT) ok
+I (20) ECB: 0x3ffb7d36   fa 61 03 78 74 ea 40 23  83 c5 f3 e3 30 d8 b5 58  |.a.xt.@#....0..X|
+I (20) ECB: mbedtls_aes_crypt_ecb(MBEDTLS_AES_DECRYPT) ok
+I (30) ECB: 0x3ffb7d26   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+```
---
+++ aes_crypt/main/main.c
@@ -0,0 +1,259 @@
+/* The example of ESP-IDF
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+/* Cryptoauthlib includes */
+//#include "cryptoauthlib.h"
+//#include "mbedtls/atca_mbedtls_wrap.h"
+
+/* mbedTLS includes */
+#include "mbedtls/platform.h"
+#include "mbedtls/debug.h"
+#include "mbedtls/ssl.h"
+#include "mbedtls/entropy.h"
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/pk.h"
+
+#define TAG "MAIN"
+
+void aes_cfb128(void *pvParameters)
+{
+	int ret;
+
+	mbedtls_entropy_context entropy;
+	mbedtls_entropy_init( &entropy );
+
+	mbedtls_ctr_drbg_context ctr_drbg;
+	mbedtls_ctr_drbg_init( &ctr_drbg );
+
+	// sets up the CTR_DRBG entropy source for future reseeds.
+	const char * seed = "some random seed string";
+	ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+				(const unsigned char *)seed, strlen(seed));
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_ctr_drbg_seed failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_ctr_drbg_seed ok");
+	}
+
+	// Generate AES key
+	// The AES key is a random bit string of appropriate length.
+	// A 128-bit AES key requires 16 bytes.
+	// A 256-bit AES key requires 32 bytes. 
+	unsigned char key[16];
+	ret = mbedtls_ctr_drbg_random( &ctr_drbg, key, 16 );
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_ctr_drbg_random failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_ctr_drbg_random ok");
+	}
+
+	// initializes the specified AES context.
+	mbedtls_aes_context aes;
+	mbedtls_aes_init(&aes);
+
+	// sets the encryption key.
+	mbedtls_aes_setkey_enc( &aes, key, 256 );
+
+	unsigned char iv[16];
+	unsigned char input [128];
+	unsigned char output[128];
+	for (int i=0;i<128;i++) input[i] = i;
+
+	// performs an AES-CFB128 encryption operation.
+	size_t iv_offset = 0;
+	int crypt_len = 50;
+	memset(iv, 0x00, sizeof(iv));
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_cfb128(&aes, MBEDTLS_AES_ENCRYPT, crypt_len, &iv_offset, iv, input, output);
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_ENCRYPT) failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), output, crypt_len, ESP_LOG_INFO);
+
+	// performs an AES-CFB128 decryption operation.
+	iv_offset = 0;
+	memset(iv, 0x00, sizeof(iv));
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_cfb128(&aes, MBEDTLS_AES_DECRYPT, crypt_len, &iv_offset, iv, output, input);
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_DECRYPT) failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), input, crypt_len, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_free(&aes);
+
+	vTaskDelete(NULL);
+}
+
+void aes_cbc(void *pvParameters)
+{
+	int ret;
+
+	mbedtls_entropy_context entropy;
+	mbedtls_entropy_init( &entropy );
+
+	mbedtls_ctr_drbg_context ctr_drbg;
+	mbedtls_ctr_drbg_init( &ctr_drbg );
+
+	// sets up the CTR_DRBG entropy source for future reseeds.
+	const char * seed = "some random seed string";
+	ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+				(const unsigned char *)seed, strlen(seed));
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_ctr_drbg_seed failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_ctr_drbg_seed ok");
+	}
+
+	// Generate AES key
+	// The AES key is a random bit string of appropriate length.
+	// A 128-bit AES key requires 16 bytes.
+	// A 256-bit AES key requires 32 bytes. 
+	unsigned char key[16];
+	ret = mbedtls_ctr_drbg_random( &ctr_drbg, key, 16 );
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_ctr_drbg_random failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_ctr_drbg_random ok");
+	}
+
+	// initializes the specified AES context.
+	mbedtls_aes_context aes;
+	mbedtls_aes_init(&aes);
+
+	// sets the encryption key.
+	mbedtls_aes_setkey_enc( &aes, key, 256 );
+
+	unsigned char iv[16];
+	unsigned char input [128];
+	unsigned char output[128];
+	for (int i=0;i<128;i++) input[i] = i;
+
+	// performs an AES-CBC encryption operation.
+	// The length of the input data in Bytes. This must be a multiple of the block size.
+	int crypt_len = 16*4;
+	memset(iv, 0x00, sizeof(iv));
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, crypt_len, iv, input, output);
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_aes_crypt_cbc(MBEDTLS_AES_ENCRYPT) failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_aes_crypt_cbc(MBEDTLS_AES_ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), output, crypt_len, ESP_LOG_INFO);
+
+	// performs an AES-CBC decryption operation.
+	memset(iv, 0x00, sizeof(iv));
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, crypt_len, iv, output, input);
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_aes_crypt_cbc(MBEDTLS_AES_DECRYPT) failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_aes_crypt_cbc(MBEDTLS_AES_DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), input, crypt_len, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_free(&aes);
+
+	vTaskDelete(NULL);
+}
+
+
+void aes_ecb(void *pvParameters)
+{
+	int ret;
+
+	mbedtls_entropy_context entropy;
+	mbedtls_entropy_init( &entropy );
+
+	mbedtls_ctr_drbg_context ctr_drbg;
+	mbedtls_ctr_drbg_init( &ctr_drbg );
+
+	// sets up the CTR_DRBG entropy source for future reseeds.
+	const char * seed = "some random seed string";
+	ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+				(const unsigned char *)seed, strlen(seed));
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_ctr_drbg_seed failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_ctr_drbg_seed ok");
+	}
+
+	// Generate AES key
+	// The AES key is a random bit string of appropriate length.
+	// A 128-bit AES key requires 16 bytes.
+	// A 256-bit AES key requires 32 bytes. 
+	unsigned char key[16];
+	ret = mbedtls_ctr_drbg_random( &ctr_drbg, key, 16 );
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_ctr_drbg_random failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_ctr_drbg_random ok");
+	}
+
+	// initializes the specified AES context.
+	mbedtls_aes_context aes;
+	mbedtls_aes_init(&aes);
+
+	// sets the encryption key.
+	mbedtls_aes_setkey_enc( &aes, key, 256 );
+
+	unsigned char input [16];
+	unsigned char output[16];
+	for (int i=0;i<16;i++) input[i] = i;
+
+	// performs an AES-SINGLE-BLOCK encryption operation.
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, input, output);
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_aes_crypt_ecb(MBEDTLS_AES_ENCRYPT) failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_aes_crypt_ecb(MBEDTLS_AES_ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), output, 16, ESP_LOG_INFO);
+
+	// performs an AES-SINGLE-BLOCK decryption operation.
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_DECRYPT, output, input);
+	if (ret != 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "mbedtls_aes_crypt_ecb(MBEDTLS_AES_DECRYPT) failed %d", ret);
+	} else {
+		ESP_LOGI(pcTaskGetName(NULL), "mbedtls_aes_crypt_ecb(MBEDTLS_AES_DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), input, 16, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_free(&aes);
+	vTaskDelete(NULL);
+}
+
+void app_main()
+{
+#if CONFIG_AES_CFB128
+	xTaskCreate(&aes_cfb128, "CFB128", 1024*4, NULL, 5, NULL);
+#endif
+
+#if CONFIG_AES_CBC
+	xTaskCreate(&aes_cbc, "CBC", 1024*4, NULL, 5, NULL);
+#endif
+
+#if CONFIG_AES_ECB
+	xTaskCreate(&aes_ecb, "ECB", 1024*4, NULL, 5, NULL);
+#endif
+}
+
