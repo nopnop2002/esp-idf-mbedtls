97d6a0db2ea4d4f608d633b1b36a0e243fbf8f35
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-09-18 12:28:50 +0900

    fit to esp-idf v5

--- md5/README.md
+++ md5/README.md
@@ -1,15 +1,5 @@
 # Generate md5 digest from a string
 
-esp-idf contains md5 functions that are different from mbedtls.   
-API differs depending on the version of esp-idf.   
-|v4.3|v4.4|
-|:-:|:-:|
-|MD5Init|esp_rom_md5_init|
-|MD5Update|esp_rom_md5_update|
-|MD5Final|esp_rom_md5_final|
-
-Here is the demo code.   
-
 ```
 MD5('Hello, world!') = 6c d3 55 6d eb 0d a5 4b ca 06 0b 4c 39 47 98 39
 ```
--- md5/main/main.c
+++ md5/main/main.c
@@ -6,27 +6,19 @@
 
 #include "freertos/FreeRTOS.h"
 
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
-#define MD5Init esp_rom_md5_init
-#define MD5Update esp_rom_md5_update
-#define MD5Final esp_rom_md5_final
 #include "esp_rom_md5.h"
-#else
-#include "esp32/rom/md5_hash.h"
-#endif // ESP_IDF_VERSION
-
 
 void app_main()
 {
 	struct MD5Context context;
-	MD5Init(&context);
+	esp_rom_md5_init(&context);
 
 	unsigned char text[64];
 	strcpy((char *)text, "Hello, world!");
-	MD5Update(&context, text, strlen((char *)text));
+	esp_rom_md5_update(&context, text, strlen((char *)text));
 	
 	unsigned char digest[16];
-	MD5Final(digest, &context);
+	esp_rom_md5_final(digest, &context);
 	printf( "\n  MD5('%s') = ", text );
 	for(int i=0;i<16;i++) {
 		printf("%02x ", digest[i]);
