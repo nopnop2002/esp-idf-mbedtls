eea016bbf56bc2b663607bb506db5d8fe777ff6a
nopnop2002 <>
2025-05-12 22:25:55 +0900

    Added aes_cfb8/aes_ctr/aes_ofb/aes_xts

--- aes_crypt/README.md
+++ aes_crypt/README.md
@@ -4,14 +4,34 @@ This project uses mbedtls_ctr_drbg_seed and mbedtls_ctr_drbg_random to generate 
 
 I referred [this](https://tls.mbed.org/kb/how-to/encrypt-with-aes-cbc).   
 
+|Mechanism|ESP-IDF|InputSize|
+|:-:|:-:|:-:|
+|AES_ECB|Supported|16 Bytes|
+|AES_CBC|Supported|16*n Bytes|
+|AES_CBC_PAD|Not Supported|Any|
+|AES_OFB|Supported|Any|
+|AES_CFB8|Supported|Any|
+|AES_CFB64|Not Supported|Any|
+|AES_CFB128|Supported|Any|
+|AES_CTS|Not Supported|=>16 Bytes|
+|AES_CTR|Supported|Any|
+|AES_XTS|Supported|Any|
 
-# Select cryption   
-![Image](https://github.com/user-attachments/assets/7b408270-c271-410d-b7a8-249d2cb73369)
-![Image](https://github.com/user-attachments/assets/a3e4c67d-c315-4b54-ba74-cfc46d94d4bd)
+# AES_CFB8   
+```
+I (313) aes_cfb8: mbedtls_aes_crypt_cfb8(ENCRYPT) ok
+I (313) aes_cfb8: 0x3ffb3bd2   69 3f 86 04 1c 77 ad 9d  49 fd c6 06 56 db a6 50  |i?...w..I...V..P|
+I (323) aes_cfb8: 0x3ffb3be2   cc e1 13 bf fc 67 b3 58  f5 8d 8c db d5 70 1b c6  |.....g.X.....p..|
+I (333) aes_cfb8: 0x3ffb3bf2   23 85 b5 7d 52 e7 13 7f  0e af 71 a7 5c c0 36 c3  |#..}R.....q.\.6.|
+I (343) aes_cfb8: 0x3ffb3c02   ee 22                                             |."|
+I (343) aes_cfb8: mbedtls_aes_crypt_cfb8(DECRYPT) ok
+I (353) aes_cfb8: 0x3ffb3b52   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (363) aes_cfb8: 0x3ffb3b62   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (373) aes_cfb8: 0x3ffb3b72   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !"#$%&'()*+,-./|
+I (373) aes_cfb8: 0x3ffb3b82   30 31                                             |01|
+```
 
-
-# AES-CFB128   
-AES-CFB128 encryption or decryption operation.   
+# AES_CFB128   
 ```
 I (333) aes_cfb128: mbedtls_aes_crypt_cfb128(MBEDTLS_AES_ENCRYPT) ok
 I (343) aes_cfb128: 0x3ffb3c42   2e 4b 1d 98 9b 30 c6 20  b4 d6 06 f9 87 73 66 7e  |.K...0. .....sf~|
@@ -25,10 +45,7 @@ I (403) aes_cfb128: 0x3ffb3be2   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2
 I (413) aes_cfb128: 0x3ffb3bf2   30 31                                             |01|
 ```
 
-
-# AES-CBC   
-AES-CBC encryption or decryption operation on full blocks.   
-This must be a multiple of the block size (16*n Bytes).
+# AES_CBC   
 ```
 I (423) aes_cbc: mbedtls_aes_crypt_cbc(MBEDTLS_AES_ENCRYPT) ok
 I (433) aes_cbc: 0x3ffb3c42   80 e1 89 e4 d1 f0 3f 98  48 12 0f 26 a5 b6 40 8d  |......?.H..&..@.|
@@ -43,11 +60,52 @@ I (503) aes_cbc: 0x3ffb3bf2   30 31 32 33 34 35 36 37  38 39 3a 3b 3c 3d 3e 3f  
 ```
 
 
-# AES-ECB   
-AES-ECB single-block (16 Bytes) encryption or decryption.   
+# AES_ECB   
 ```
 I (513) aes_ecb: mbedtls_aes_crypt_ecb(MBEDTLS_AES_ENCRYPT) ok
 I (523) aes_ecb: 0x3ffb3cb2   80 e1 89 e4 d1 f0 3f 98  48 12 0f 26 a5 b6 40 8d  |......?.H..&..@.|
 I (533) aes_ecb: mbedtls_aes_crypt_ecb(MBEDTLS_AES_DECRYPT) ok
 I (533) aes_ecb: 0x3ffb3ca2   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+```
+
+# AES_CTR   
+```
+I (573) aes_ctr: mbedtls_aes_crypt_ctr(ENCRYPT) ok
+I (573) aes_ctr: 0x3ffb3bd8   69 ae 72 34 7d 62 ef ca  bf fe 33 4c d9 03 33 b5  |i.r4}b....3L..3.|
+I (583) aes_ctr: 0x3ffb3be8   00 ac b8 01 4e 09 7f 21  82 9d 6d 73 58 9e a2 33  |....N..!..msX..3|
+I (593) aes_ctr: 0x3ffb3bf8   9d 36 af db b9 7c e2 56  bf 9c cc 80 24 8a a1 c7  |.6...|.V....$...|
+I (603) aes_ctr: 0x3ffb3c08   4c 04                                             |L.|
+I (603) aes_ctr: mbedtls_aes_crypt_ctr(DECRYPT) ok
+I (613) aes_ctr: 0x3ffb3b58   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (623) aes_ctr: 0x3ffb3b68   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (633) aes_ctr: 0x3ffb3b78   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !"#$%&'()*+,-./|
+I (633) aes_ctr: 0x3ffb3b88   30 31
+```
+
+# AES_OFB   
+```
+I (643) aes_ofb: mbedtls_aes_crypt_ofb(ENCRYPT) ok
+I (653) aes_ofb: 0x3ffb3bd2   69 ae 72 34 7d 62 ef ca  bf fe 33 4c d9 03 33 b5  |i.r4}b....3L..3.|
+I (663) aes_ofb: 0x3ffb3be2   1f 00 6c 5a 9e f7 ee d8  9e 3f be 3c cf 66 16 dd  |..lZ.....?.<.f..|
+I (663) aes_ofb: 0x3ffb3bf2   5a 51 fd c8 78 29 dd 91  c1 de 23 c3 60 b4 75 02  |ZQ..x)....#.`.u.|
+I (673) aes_ofb: 0x3ffb3c02   17 ce                                             |..|
+I (683) aes_ofb: mbedtls_aes_crypt_ofb(DECRYPT) ok
+I (683) aes_ofb: 0x3ffb3b52   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (693) aes_ofb: 0x3ffb3b62   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (703) aes_ofb: 0x3ffb3b72   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !"#$%&'()*+,-./|
+I (713) aes_ofb: 0x3ffb3b82   30 31
+```
+
+# AES_XTS   
+```
+I (723) aes_xts: mbedtls_aes_crypt_xts(ENCRYPT) ok
+I (723) aes_xts: 0x3ffb3bd4   e9 82 5b 8b 4a 15 f9 b9  0a af a4 a3 f9 9b 6c cc  |..[.J.........l.|
+I (733) aes_xts: 0x3ffb3be4   3c cf fc 68 52 04 79 19  28 aa b7 a7 bb e0 31 e0  |<..hR.y.(.....1.|
+I (743) aes_xts: 0x3ffb3bf4   e4 fa fa 05 4c 89 a5 98  f5 d2 b3 62 85 8b f3 a8  |....L......b....|
+I (753) aes_xts: 0x3ffb3c04   59 10                                             |Y.|
+I (763) aes_xts: mbedtls_aes_crypt_xts(DECRYPT) ok
+I (763) aes_xts: 0x3ffb3b54   00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|
+I (773) aes_xts: 0x3ffb3b64   10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|
+I (783) aes_xts: 0x3ffb3b74   20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !"#$%&'()*+,-./|
+I (793) aes_xts: 0x3ffb3b84   30 31
 ```
--- aes_crypt/main/main.c
+++ aes_crypt/main/main.c
@@ -24,6 +24,52 @@
 
 static const char *TAG = "MAIN";
 
+esp_err_t aes_cfb8(unsigned char *key)
+{
+	int ret;
+
+	// initializes the specified AES context.
+	mbedtls_aes_context aes;
+	mbedtls_aes_init(&aes);
+
+	// sets the encryption key.
+	mbedtls_aes_setkey_enc( &aes, key, 256 );
+
+	unsigned char iv[16];
+	unsigned char input [128];
+	unsigned char output[128];
+	for (int i=0;i<128;i++) input[i] = i;
+
+	// performs an AES-CFB8 encryption operation.
+	int crypt_len = 50;
+	memset(iv, 0x00, sizeof(iv));
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_cfb8(&aes, MBEDTLS_AES_ENCRYPT, crypt_len, iv, input, output);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cfb8(ENCRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cfb8(ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, crypt_len, ESP_LOG_INFO);
+
+	// performs an AES-CFB8 decryption operation.
+	memset(iv, 0x00, sizeof(iv));
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_cfb8(&aes, MBEDTLS_AES_DECRYPT, crypt_len, iv, output, input);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cfb8(DECRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cfb8(DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, crypt_len, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_free(&aes);
+	return ESP_OK;
+}
+
 esp_err_t aes_cfb128(unsigned char *key)
 {
 	int ret;
@@ -47,10 +93,10 @@ esp_err_t aes_cfb128(unsigned char *key)
 	memset(output, 0x00, sizeof(output));
 	ret = mbedtls_aes_crypt_cfb128(&aes, MBEDTLS_AES_ENCRYPT, crypt_len, &iv_offset, iv, input, output);
 	if (ret != 0) {
-		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_ENCRYPT) failed %d", ret);
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cfb128(ENCRYPT) failed %d", ret);
 		return ESP_FAIL;
 	} else {
-		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_ENCRYPT) ok");
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cfb128(ENCRYPT) ok");
 	}
 	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, crypt_len, ESP_LOG_INFO);
 
@@ -60,10 +106,10 @@ esp_err_t aes_cfb128(unsigned char *key)
 	memset(input, 0x00, sizeof(input));
 	ret = mbedtls_aes_crypt_cfb128(&aes, MBEDTLS_AES_DECRYPT, crypt_len, &iv_offset, iv, output, input);
 	if (ret != 0) {
-		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_DECRYPT) failed %d", ret);
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cfb128(DECRYPT) failed %d", ret);
 		return ESP_FAIL;
 	} else {
-		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cfb128(MBEDTLS_AES_DECRYPT) ok");
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cfb128(DECRYPT) ok");
 	}
 	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, crypt_len, ESP_LOG_INFO);
 
@@ -95,10 +141,10 @@ esp_err_t aes_cbc(unsigned char *key)
 	memset(output, 0x00, sizeof(output));
 	ret = mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, crypt_len, iv, input, output);
 	if (ret != 0) {
-		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cbc(MBEDTLS_AES_ENCRYPT) failed %d", ret);
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cbc(ENCRYPT) failed %d", ret);
 		return ESP_FAIL;
 	} else {
-		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cbc(MBEDTLS_AES_ENCRYPT) ok");
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cbc(ENCRYPT) ok");
 	}
 	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, crypt_len, ESP_LOG_INFO);
 
@@ -107,10 +153,10 @@ esp_err_t aes_cbc(unsigned char *key)
 	memset(input, 0x00, sizeof(input));
 	ret = mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, crypt_len, iv, output, input);
 	if (ret != 0) {
-		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cbc(MBEDTLS_AES_DECRYPT) failed %d", ret);
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_cbc(DECRYPT) failed %d", ret);
 		return ESP_FAIL;
 	} else {
-		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cbc(MBEDTLS_AES_DECRYPT) ok");
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_cbc(DECRYPT) ok");
 	}
 	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, crypt_len, ESP_LOG_INFO);
 
@@ -138,10 +184,10 @@ esp_err_t aes_ecb(unsigned char *key)
 	memset(output, 0x00, sizeof(output));
 	ret = mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, input, output);
 	if (ret != 0) {
-		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ecb(MBEDTLS_AES_ENCRYPT) failed %d", ret);
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ecb(ENCRYPT) failed %d", ret);
 		return ESP_FAIL;
 	} else {
-		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ecb(MBEDTLS_AES_ENCRYPT) ok");
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ecb(ENCRYPT) ok");
 	}
 	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, 16, ESP_LOG_INFO);
 
@@ -149,15 +195,160 @@ esp_err_t aes_ecb(unsigned char *key)
 	memset(input, 0x00, sizeof(input));
 	ret = mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_DECRYPT, output, input);
 	if (ret != 0) {
-		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ecb(MBEDTLS_AES_DECRYPT) failed %d", ret);
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ecb(DECRYPT) failed %d", ret);
 		return ESP_FAIL;
 	} else {
-		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ecb(MBEDTLS_AES_DECRYPT) ok");
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ecb(DECRYPT) ok");
 	}
 	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, 16, ESP_LOG_INFO);
 
 	// releases and clears the specified AES context.
 	mbedtls_aes_free(&aes);
+	return ESP_OK;
+}
+
+esp_err_t aes_ctr(unsigned char *key)
+{
+	int ret;
+
+	// initializes the specified AES context.
+	mbedtls_aes_context aes;
+	mbedtls_aes_init(&aes);
+
+	// sets the encryption key.
+	mbedtls_aes_setkey_enc( &aes, key, 256 );
+
+	size_t nc_off = 0;
+	unsigned char nonce_counter[16];
+	unsigned char stream_block[16];
+	unsigned char input [128];
+	unsigned char output[128];
+	for (int i=0;i<128;i++) input[i] = i;
+
+	// performs an AES-CTR encryption operation.
+	int crypt_len = 50;
+	memset(nonce_counter, 0x00, sizeof(nonce_counter));
+	memset(stream_block, 0x00, sizeof(stream_block));
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_ctr(&aes, crypt_len, &nc_off, nonce_counter, stream_block, input, output);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ctr(ENCRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ctr(ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, crypt_len, ESP_LOG_INFO);
+
+	// performs an AES-CTR decryption operation.
+	nc_off = 0;
+	memset(nonce_counter, 0x00, sizeof(nonce_counter));
+	memset(stream_block, 0x00, sizeof(stream_block));
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_ctr(&aes, crypt_len, &nc_off, nonce_counter, stream_block, output, input);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ctr(DECRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ctr(DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, crypt_len, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_free(&aes);
+	return ESP_OK;
+}
+
+esp_err_t aes_ofb(unsigned char *key)
+{
+	int ret;
+
+	// initializes the specified AES context.
+	mbedtls_aes_context aes;
+	mbedtls_aes_init(&aes);
+
+	// sets the encryption key.
+	mbedtls_aes_setkey_enc( &aes, key, 256 );
+
+	unsigned char iv[16];
+	unsigned char input [128];
+	unsigned char output[128];
+	for (int i=0;i<128;i++) input[i] = i;
+
+	// performs an AES-OFB encryption operation.
+	size_t iv_offset = 0;
+	int crypt_len = 50;
+	memset(iv, 0x00, sizeof(iv));
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_ofb(&aes, crypt_len, &iv_offset, iv, input, output);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ofb(ENCRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ofb(ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, crypt_len, ESP_LOG_INFO);
+
+	// performs an AES-OFB decryption operation.
+	iv_offset = 0;
+	memset(iv, 0x00, sizeof(iv));
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_ofb(&aes, crypt_len, &iv_offset, iv, output, input);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_ofb(DECRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_ofb(DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, crypt_len, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_free(&aes);
+	return ESP_OK;
+}
+
+esp_err_t aes_xts(unsigned char *key)
+{
+	int ret;
+
+	// initializes the specified AES context.
+	mbedtls_aes_xts_context ctx;
+	mbedtls_aes_xts_init(&ctx);
+
+	// sets the encryption key.
+	mbedtls_aes_xts_setkey_enc( &ctx, key, 256 );
+
+	unsigned char data_unit[16];
+	unsigned char input [128];
+	unsigned char output[128];
+	for (int i=0;i<128;i++) input[i] = i;
+
+	// performs an AES-OFB encryption operation.
+	int crypt_len = 50;
+	memset(data_unit, 0x00, sizeof(data_unit));
+	memset(output, 0x00, sizeof(output));
+	ret = mbedtls_aes_crypt_xts(&ctx, MBEDTLS_AES_ENCRYPT, crypt_len, data_unit, input, output);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_xts(ENCRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_xts(ENCRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, output, crypt_len, ESP_LOG_INFO);
+
+	// performs an AES-OFB decryption operation.
+	memset(data_unit, 0x00, sizeof(data_unit));
+	memset(input, 0x00, sizeof(input));
+	ret = mbedtls_aes_crypt_xts(&ctx, MBEDTLS_AES_DECRYPT, crypt_len, data_unit, output, input);
+	if (ret != 0) {
+		ESP_LOGE(__FUNCTION__, "mbedtls_aes_crypt_xts(DECRYPT) failed %d", ret);
+		return ESP_FAIL;
+	} else {
+		ESP_LOGI(__FUNCTION__, "mbedtls_aes_crypt_xts(DECRYPT) ok");
+	}
+	ESP_LOG_BUFFER_HEXDUMP(__FUNCTION__, input, crypt_len, ESP_LOG_INFO);
+
+	// releases and clears the specified AES context.
+	mbedtls_aes_xts_free(&ctx);
 	return ESP_OK;
 }
 
@@ -194,17 +385,18 @@ void app_main()
 	}
 	ESP_LOG_BUFFER_HEXDUMP(TAG, key, 32, ESP_LOG_INFO);
 
+	ESP_ERROR_CHECK(aes_cfb8(key));
 
-#if CONFIG_AES_CFB128
 	ESP_ERROR_CHECK(aes_cfb128(key));
-#endif
 
-#if CONFIG_AES_CBC
 	ESP_ERROR_CHECK(aes_cbc(key));
-#endif
 
-#if CONFIG_AES_ECB
 	ESP_ERROR_CHECK(aes_ecb(key));
-#endif
+
+	ESP_ERROR_CHECK(aes_ctr(key));
+
+	ESP_ERROR_CHECK(aes_ofb(key));
+
+	ESP_ERROR_CHECK(aes_xts(key));
 }
 
